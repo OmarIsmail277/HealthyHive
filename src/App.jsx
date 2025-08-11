64b696094b0d0a2cce59b96283e0320b744a42f6
MoeMohsenM <moemohsen@outlook.com>
2025-08-11 21:42:32 +0300

    category tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./Routes/AppRoutes";
 
@@ -7,20 +6,6 @@ function App() {
     <BrowserRouter>
       <AppRoutes />
     </BrowserRouter>
-=======
-// import { BrowserRouter } from "react-router-dom";
-
-// function App() {
-//   return <BrowserRouter>{/* Put your Components here */}</BrowserRouter>;
-// }
-// export default App;
-
-import Cart from "./components/Cart/Cart";
-
-function App() {
-  return (
-    <Cart/>
->>>>>>> Stashed changes
   );
 }
 export default App;
