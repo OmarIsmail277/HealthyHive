3d538870a8ea7e3da8c559e28c3b1c3db992a2bc
Omar Ahmed <omar.ismail277@gmail.com>
2025-08-18 00:26:54 +0300

    Fetched Products!

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./Routes/AppRoutes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
+
+// In the next lecture, we will start fetching some data.
+// And so then that will show up right there in the Dev Tools. And this data will then be available because we provided it here using this,"QueryClientProvider." So again, just like we did with Redux and with the context API before. So a similar idea of having the data in one place and then providing it to the whole component tree.
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 export default App;
